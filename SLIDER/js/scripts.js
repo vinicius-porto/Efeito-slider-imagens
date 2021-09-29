1beb304233b082563f782d7fc761a92b475f81d7
Vinicius Porto <55770422+vinicius-porto@users.noreply.github.com>
2021-09-29 19:11:52 -0300

    Add files via upload

---
+++ SLIDER/js/scripts.js
@@ -0,0 +1,58 @@
+const slider1 = document.querySelector("#slider1");
+slider1.addEventListener("click", function myslider1() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider1.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+const slider2 = document.querySelector("#slider2");
+slider2.addEventListener("click", function myslider2() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider2.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+const slider3 = document.querySelector("#slider3");
+slider3.addEventListener("click", function myslider3() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider3.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+const slider4 = document.querySelector("#slider4");
+slider4.addEventListener("click", function myslider4() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider4.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+const slider5 = document.querySelector("#slider5");
+slider5.addEventListener("click", function myslider5() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider5.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+
+const slider6 = document.querySelector("#slider6");
+slider6.addEventListener("click", function myslider6() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider6.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+const slider7 = document.querySelector("#slider7");
+slider7.addEventListener("click", function myslider7() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/slider7.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
+
+const sliderstart = document.querySelector("#slider-start");
+sliderstart.addEventListener("click", function sliderStart() {
+  document.getElementById("slider").style.backgroundImage =
+    "url('./img/start.jpg')";
+  document.getElementById("slider").style.backgroundPosition = "center";
+  document.getElementById("slider").style.backgroundSize = "cover";
+});
